590ae28c2ee3bb11301e375bef8c48012d408c7b
LudwigEth <ludwig.eth@gmail.com>
2023-08-25 20:32:59 +0200

    Feat: add function "addition" for addition

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const topScreen = document.getElementById("top-screen");
 const bottomScreen = document.getElementById("bottom-screen");
+
+
+button.addEventListener("click", function() {
+
+})
+
+
+const addition = (a, b) => a + b;
