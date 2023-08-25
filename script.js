4512c39082870d89ba138e47102e7d4bea580d59
LudwigEth <ludwig.eth@gmail.com>
2023-08-25 21:15:55 +0200

    Feat: add function operate(numA, numB, operator)
    function that will perform a mathematical operation
    with numA and numB depending on the used operator

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const topScreen = document.getElementById("top-screen");
 const bottomScreen = document.getElementById("bottom-screen");
-
+let numA;
+let numB;
+let operator;
 
 button.addEventListener("click", function() {
 
@@ -14,3 +16,21 @@ const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+
+const operate = (numA, numB, operator) => {
+    switch (operator) {
+        case "+":
+            add(numA, numB);
+            break;
+        case "-":
+            substract(numA, numB);
+            break;
+        case "×":
+            multiply(numA, numB);
+            break;
+        case "+":
+            divide(numA, numB);
+            break;
+    }
+}
